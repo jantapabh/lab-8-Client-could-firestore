abdbf9bfd9d904e1b706832b1a2475aced4bf3d5
jantapabh <s6035512034@phuket.psu.ac.th>
2020-03-19 10:28:50 +0700

    ad d commit err

--- src/App.js
+++ src/App.js
@@ -1,27 +1,80 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
+import { firestore } from './index'
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const [task, setTask] = useState([
+    { id: 1, name: "do homewoke" },
+    { id: 2, name: "Write Nodejs" }
+
+  ])
+
+  const [name, setName] = useState('')
+
+  const retriveData = () => {
+
+    firestore.collection("task".onSnapshotS((snapshot) => {
+
+      console.log(snapshot);
+    let myTask =  snapshot.docs.map( d => {
+        const {id, name} = d.data()
+        console.log(id, name)
+        return {id, name}
+      })
+      setTask(myTask)
+      
+
+    }))
+  }
+
+
+  useEffect(() => {
+
+  
+    retriveData()
+
+  }, [])
+
+  const renderTask = () => {
+
+    if (task && task.length != 0) {
+
+      return (
+        task.map((task, index) => {
+          <li key={index}>{task.id} : {task.name}</li>
+        })
+      )
+    }
+  else {
+      return (
+
+        <li>No task</li>
+      )
+    }
+  }
+
+  const addTask = () => {
+    let id = task[task.length-1].id+1;
+     firestore.collection("tasks").docs(id).set({id, name})
+
+  }
+
+
+return (
+
+  <div>
+    <h1>To do</h1>
+    <input type="text" name="name" onChange={(e) => setName(e.target.value)} />
+    <button onClick={addTask}>SUB</button>
+    <ul>{renderTask()}</ul>
+  </div>
+
+)
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+var firebaseConfig = {
+    apiKey: "AIzaSyDOjl-c68si7yeN-9inz182bW7PuHcSHCQ",
+    authDomain: "todo-240-311.firebaseapp.com",
+    databaseURL: "https://todo-240-311.firebaseio.com",
+    projectId: "todo-240-311",
+    storageBucket: "todo-240-311.appspot.com",
+    messagingSenderId: "954762069870",
+    appId: "1:954762069870:web:47b5371e51b4a520dfe37f",
+    measurementId: "G-HN91FPJX1F"
+  };
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
